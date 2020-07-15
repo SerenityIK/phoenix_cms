491b65b8f4fa88e0d4016452d8a9215346a2f3ee
serenity <serenity597th@gmail.com>
2020-07-15 13:05:21 +0300

    Add routes

--- lib/phoenix_cms_web/router.ex
+++ lib/phoenix_cms_web/router.ex
@@ -17,26 +17,20 @@ defmodule PhoenixCmsWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources("/blog", BlogController, only: [:index, :show])
+    resources("/session", SessionController, only: [:create, :new, :delete])
+  end
+
+  scope "/admin", PhoenixCmsWeb, as: :admin do
+    pipe_through :browser
+    get "/", Admin.HomeController, :index
+    resources("/post", Admin.Postcontroller) do
+      get "/publish", Admin.Postcontroller, :publish, as: :publish
+    end
   end
 
   # Other scopes may use custom stacks.
   # scope "/api", PhoenixCmsWeb do
   #   pipe_through :api
   # end
-
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
-
-    scope "/" do
-      pipe_through :browser
-      live_dashboard "/dashboard", metrics: PhoenixCmsWeb.Telemetry
-    end
-  end
 end
